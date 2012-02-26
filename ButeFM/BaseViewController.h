d5a8451d3d9af86a1c4663a4fb990ce3fbf69074
Iain Smith <b099l3@gmail.com>
2012-02-26 21:36:48 +0000

    tab bar almost working centre button seected state

--- ButeFM/BaseViewController.h
+++ ButeFM/BaseViewController.h
@@ -25,14 +25,19 @@
 // THE SOFTWARE
 //
 
-@interface BaseViewController : UITabBarController
+@interface BaseViewController : UITabBarController <UITabBarDelegate>
 {
+    
 }
 
 // Create a view controller and setup it's tab bar item with a title and image
 -(UIViewController*) viewControllerWithTabTitle:(NSString*)title image:(UIImage*)image;
 
 // Create a custom UIButton and add it to the center of our tab bar
--(void) addCenterButtonWithImage:(UIImage*)buttonImage highlightImage:(UIImage*)highlightImage;
+-(void) addCenterButtonWithImage:(UIImage*)buttonImage highlightImage:(UIImage*)highlightImage selectedImage:(UIImage*)selectedImage;
+
+-(IBAction)centreButtonPressed:(UIButton *)sender;
+
+- (void)tabBar:(UITabBar *)tabBar didSelectItem:(UITabBarItem *)item;
 
 @end
--- ButeFM/BaseViewController.m
+++ ButeFM/BaseViewController.m
@@ -1,4 +1,4 @@
-    //
+//
 //  BaseViewController.m
 //  RaisedCenterTabBar
 //
@@ -29,42 +29,68 @@
 
 @implementation BaseViewController
 
+- (void)viewDidLoad{
+    [super viewDidLoad];
+    self.tabBar.delegate = self;
+    [self addCenterButtonWithImage:[UIImage imageNamed:@"buteFMButton.png"] highlightImage:[UIImage imageNamed:@"buteFMButtonH.png"] selectedImage:[UIImage imageNamed:@"buteFMButtonH.png"]];
+    self.tabBar.selectedImageTintColor = [UIColor colorWithRed:0.95 green:0.5 blue:0.15 alpha:1.0];
+}
+
+-(IBAction)buteButtonPush:(UIButton *)sender{
+    
+}
+
 // Create a view controller and setup it's tab bar item with a title and image
 -(UIViewController*) viewControllerWithTabTitle:(NSString*) title image:(UIImage*)image
 {
-  UIViewController* viewController = [[UIViewController alloc] init];
-  viewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:title image:image tag:0];
-  return viewController;
+    UIViewController* viewController = [[UIViewController alloc] init];
+    viewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:title image:image tag:0];
+    return viewController;
 }
 
 // Create a custom UIButton and add it to the center of our tab bar
--(void) addCenterButtonWithImage:(UIImage*)buttonImage highlightImage:(UIImage*)highlightImage
+-(void) addCenterButtonWithImage:(UIImage*)buttonImage highlightImage:(UIImage*)highlightImage selectedImage:(UIImage*)selectedImage
 {
-  UIButton* button = [UIButton buttonWithType:UIButtonTypeCustom];
-  button.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin;
-  button.frame = CGRectMake(0.0, 0.0, buttonImage.size.width, buttonImage.size.height);
-  [button setBackgroundImage:buttonImage forState:UIControlStateNormal];
-  [button setBackgroundImage:highlightImage forState:UIControlStateHighlighted];
-
-  CGFloat heightDifference = buttonImage.size.height - self.tabBar.frame.size.height;
-  if (heightDifference < 0)
-    button.center = self.tabBar.center;
-  else
-  {
-    CGPoint center = self.tabBar.center;
-    center.y = center.y - heightDifference/2.0;
-    button.center = center;
-  }
-
-   
-  [self.view addSubview:button];
+    UIButton* button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin;
+    button.frame = CGRectMake(0.0, 0.0, buttonImage.size.width, buttonImage.size.height);
+    [button setBackgroundImage:buttonImage forState:UIControlStateNormal];
+    [button setBackgroundImage:highlightImage forState:UIControlStateHighlighted];
+    [button setBackgroundImage:selectedImage forState:UIControlStateSelected];
+    CGFloat heightDifference = buttonImage.size.height - self.tabBar.frame.size.height;
+    if (heightDifference < 0)
+        button.center = self.tabBar.center;
+    else
+    {
+        CGPoint center = self.tabBar.center;
+        center.y = center.y - heightDifference/2.0;
+        button.center = center;
+    }
+    
+    [button addTarget:self action:@selector(centreButtonPressed:) forControlEvents:UIControlEventTouchDown];    
+    [self.view addSubview:button];
 }
 
+-(IBAction)centreButtonPressed:(UIButton *)sender {
+    [self setSelectedIndex:2];
+    if ( sender.selected ) {
+        sender.highlighted = NO;
+        sender.selected = NO;
+    } else {
+        sender.highlighted = YES;
+        sender.selected = YES;
+    }
+    
+}
+
+- (void)tabBar:(UITabBar *)tabBar didSelectItem:(UITabBarItem *)item {
+    NSLog(@"tab selected: %@", item.title);
+}
 
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-  return YES;
+    return YES;
 }
 
 @end
