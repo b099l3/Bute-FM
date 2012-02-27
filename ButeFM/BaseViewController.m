a7035edcb2001f74b11ebfac98a60cfe9adef925
b099l3 <b099l3@Mac-Pro.local>
2012-02-27 19:45:06 +0000

    tab bat fully operational

--- ButeFM/BaseViewController.m
+++ ButeFM/BaseViewController.m
@@ -2,85 +2,60 @@
 //  BaseViewController.m
 //  RaisedCenterTabBar
 //
-//  Created by Peter Boctor on 12/15/10.
+//  Created by Iain Smith on 03/01/2012.
+//  Copyright (c) 2012 b099l3. All rights reserved.
 //
-// Copyright (c) 2011 Peter Boctor
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE
-//
+
 
 #import "BaseViewController.h"
 
 @implementation BaseViewController
 
+@synthesize centreButton = _centreButton;
+
 - (void)viewDidLoad{
     [super viewDidLoad];
-    self.tabBar.delegate = self;
     [self addCenterButtonWithImage:[UIImage imageNamed:@"buteFMButton.png"] highlightImage:[UIImage imageNamed:@"buteFMButtonH.png"] selectedImage:[UIImage imageNamed:@"buteFMButtonH.png"]];
-    self.tabBar.selectedImageTintColor = [UIColor colorWithRed:0.95 green:0.5 blue:0.15 alpha:1.0];
-}
-
-// Create a view controller and setup it's tab bar item with a title and image
--(UIViewController*) viewControllerWithTabTitle:(NSString*) title image:(UIImage*)image
-{
-    UIViewController* viewController = [[UIViewController alloc] init];
-    viewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:title image:image tag:0];
-    return viewController;
+    self.tabBar.selectedImageTintColor = [UIColor colorWithRed:0.956 green:0.5725 blue:0.0 alpha:1.0];     // 241 106 35 //	242	145	0	//244	146	0	244	146	0	
 }
 
 // Create a custom UIButton and add it to the center of our tab bar
 -(void) addCenterButtonWithImage:(UIImage*)buttonImage highlightImage:(UIImage*)highlightImage selectedImage:(UIImage*)selectedImage
 {
-    UIButton* button = [UIButton buttonWithType:UIButtonTypeCustom];
-    button.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin;
-    button.frame = CGRectMake(0.0, 0.0, buttonImage.size.width, buttonImage.size.height);
-    [button setBackgroundImage:buttonImage forState:UIControlStateNormal];
-    [button setBackgroundImage:highlightImage forState:UIControlStateHighlighted];
-    [button setBackgroundImage:selectedImage forState:UIControlStateSelected];
+    //UIButton* button
+    _centreButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    _centreButton.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin;
+    _centreButton.frame = CGRectMake(0.0, 0.0, buttonImage.size.width, buttonImage.size.height);
+    [_centreButton setBackgroundImage:buttonImage forState:UIControlStateNormal];
+    [_centreButton setBackgroundImage:highlightImage forState:UIControlStateHighlighted];
+    [_centreButton setBackgroundImage:selectedImage forState:UIControlStateSelected];
     CGFloat heightDifference = buttonImage.size.height - self.tabBar.frame.size.height;
     if (heightDifference < 0)
-        button.center = self.tabBar.center;
+        _centreButton.center = self.tabBar.center;
     else
     {
         CGPoint center = self.tabBar.center;
         center.y = center.y - heightDifference/2.0;
-        button.center = center;
+        _centreButton.center = center;
     }
     
-    [button addTarget:self action:@selector(centreButtonPressed:) forControlEvents:UIControlEventTouchDown];    
-    [self.view addSubview:button];
+    [_centreButton addTarget:self action:@selector(centreButtonPressed:) forControlEvents:UIControlEventTouchDown];    
+    [self.view addSubview:_centreButton];
 }
 
 -(IBAction)centreButtonPressed:(UIButton *)sender {
     [self setSelectedIndex:2];
-    if ( sender.selected ) {
-        sender.highlighted = NO;
-        sender.selected = NO;
-    } else {
-        sender.highlighted = YES;
-        sender.selected = YES;
-    }
-    
+    sender.highlighted = YES;
+    sender.selected = YES;   
+    [_centreButton setBackgroundImage:[UIImage imageNamed:@"buteFMButtonH.png"] forState:UIControlStateNormal];
 }
 
 - (void)tabBar:(UITabBar *)tabBar didSelectItem:(UITabBarItem *)item {
-    NSLog(@"tab selected: %@", item.title);
+    if (item.tag != 2) {
+        _centreButton.highlighted = NO;
+        _centreButton.selected = NO;
+         [_centreButton setBackgroundImage:[UIImage imageNamed:@"buteFMButton.png"] forState:UIControlStateNormal];
+    }
 }
 
 
