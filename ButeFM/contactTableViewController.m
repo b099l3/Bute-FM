1d706c1e02adf185bea14868dfb710c6fbdf1b64
Iain Smith <b099l3@gmail.com>
2012-03-14 23:40:58 +0000

    contacts working

--- ButeFM/contactTableViewController.m
+++ ButeFM/contactTableViewController.m
@@ -92,7 +92,7 @@
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
